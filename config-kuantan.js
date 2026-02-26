5191875f939d70efe16efb1e0055b8894fa9da76
Ben Galea <83748447+punk-primate@users.noreply.github.com>
2026-02-26 22:20:49 +0800

    Update config-kuantan.js

--- config-kuantan.js
+++ config-kuantan.js
@@ -1,12 +1,10 @@
-console.log("--- config-kuantan.js is successfully loaded ---");
-
 // --- MAPBOX CONFIGURATION ---
 const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoiYmVuZ2FsZWEiLCJhIjoiY21iNjBvbXp0MWpiejJpb2Vmc3FyeWdweSJ9.9gzKml0FN_5I30w33iqg3A';
 const MAP_STYLE_CUSTOM = 'mapbox://styles/bengalea/cmb60zyax00o501sdahv19e6q';
 const MAP_STYLE_SATELLITE = 'mapbox://styles/mapbox/satellite-streets-v12';
 
 // --- LAYER & ATTRIBUTE CONFIGURATION ---
-// Ensure this perfectly matches the name in Mapbox Studio
+// Removed the accidental leading space. 
 const FOREST_PATCH_LAYER_ID = 'Kuantan Forest Patches'; 
 
 // Updated to perfectly match the column headers from our R CSV
@@ -18,7 +16,7 @@ const CONTIGUITY_INDEX_ATTRIBUTE = 'contig';
 const PERIMETER_AREA_RATIO_ATTRIBUTE = 'para';
 const ENN_ATTRIBUTE = 'enn'; 
 
-// Attributes to display in the info panel.
+// Attributes to display in the info panel
 const INFO_PANEL_ATTRIBUTES = [
     'Tier',
     'id',
@@ -51,4 +49,3 @@ const TIER_COLORS = {
 // --- MAP INITIAL VIEW ---
 const INITIAL_CENTER = [103.3256, 3.8126];
 const INITIAL_ZOOM = 11;
-
